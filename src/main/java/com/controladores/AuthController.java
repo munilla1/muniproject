18c2ff3a4fe294aeb81ae24b4ab3aa4de7a714e5
muni <mucho-muni@hotmail.com>
2025-09-03 14:08:26 +0200

    commit para deploy

--- src/main/java/com/controladores/AuthController.java
+++ src/main/java/com/controladores/AuthController.java
@@ -13,9 +13,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping(path="api")
 public class AuthController {
 
     private final UsuarioService usuarioService;
