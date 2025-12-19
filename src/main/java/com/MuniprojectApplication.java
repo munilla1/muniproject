66d668a4c5eb9587ad5362dee33f6022b56e21cd
muni <mucho-muni@hotmail.com>
2025-12-19 11:53:47 +0100

    Implementación de recuperación de contraseña

--- src/main/java/com/MuniprojectApplication.java
+++ src/main/java/com/MuniprojectApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
--- src/main/java/com/service/UsuarioService.java
+++ src/main/java/com/service/UsuarioService.java
@@ -5,8 +5,6 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
